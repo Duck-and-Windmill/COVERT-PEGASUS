a68f6bc5385d8bc918557f77155dbd141181219e
Warren-Partridge <maps@perdix.org>
2017-09-09 03:05:00 -0400

    Add navigation to chat view

--- genie/App.js
+++ genie/App.js
@@ -1,15 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Image, Navigator, TouchableHighlight, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome',
+    };
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-end' }}>
+                <Text style={styles.container}>Hello, Welcome Screen!</Text>
+
+                <TouchableHighlight
+                    title='Genie Chat'
+                    onPress = {() => navigate('Chat')}
+                >
+                <Image style={genieButtStyles.button} source={require('./genie-butt.png')}/>
+                </TouchableHighlight>
+
+
+            </View>
+        );
+    }
 }
+
+class ChatScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Genie Chat',
+    };
+    render() {
+        return <Text style={styles.container}>Hello, Genie!</Text>;
+    }
+}
+
+const SimpleApp = StackNavigator({
+    Home: { screen: HomeScreen },
+    Chat: { screen: ChatScreen },
+});
+
+export default SimpleApp;
+
+AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
+
+
+
+
+class Message extends React.Component {
+    render() {
+        return (
+            <Text>Hello bois</Text>
+        );
+    }
+}
+
+const genieButtStyles = StyleSheet.create({
+    button: {
+        width: 70,
+        height: 70,
+        padding: 10,
+        float: right;
+    }
+});
 
 const styles = StyleSheet.create({
   container: {

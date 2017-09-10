e08fbbfd0acea1221a9beef223d9dcffa6bcfb23
Rudhra Raveendran <rudytheninja@gmail.com>
2017-09-10 00:30:35 -0400

    adding menu

--- genie/src/components/AppScreen.js
+++ genie/src/components/AppScreen.js
@@ -10,9 +10,13 @@ import {
     FlatList,
     Text
 } from 'react-native';
+import Dimensions from 'Dimensions';
 import { Actions, ActionConst } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 import Drawer from 'react-native-drawer';
+import SideMenu from 'react-native-side-menu'
+import { Card } from 'react-native-material-ui';
+import { VictoryBar } from "victory-native";
 
 import ControlPanel from './ControlPanel'
 
@@ -26,31 +30,10 @@ class AppScreen extends Component {
             isLoading: false,
             positions: []
         };
-
-        this._onPress = this._onPress.bind(this);
         this.growAnimated = new Animated.Value(0);
     }
 
     _keyExtractor = (item, index) => item.id;
-
-    _onPress() {
-        if (this.state.isLoading) return;
-
-        this.setState({ isLoading: true });
-
-        Animated.timing(
-            this.growAnimated,
-            {
-                toValue: 1,
-                duration: 300,
-                easing: Easing.linear,
-            }
-        ).start();
-
-        setTimeout(() => {
-            Actions.pop();
-        }, 500);
-    }
 
     _getAccount() {
       fetch('https://api.robinhood.com/accounts/', {
@@ -82,6 +65,9 @@ class AppScreen extends Component {
       .then((response) => response.json())
       .then((responseJson) => {
         console.log(url, responseJson);
+        responseJson.results.forEach(function(position, index, arr) {
+          arr[index].key = index
+        });
         this.setState({ positions: responseJson.results });
       })
       .catch((error) => {
@@ -114,38 +100,29 @@ class AppScreen extends Component {
     };
 
     render() {
-        const changeScale = this.growAnimated.interpolate({
-            inputRange: [0, 1],
-            outputRange: [1, SIZE],
-        });
-
         return (
-            <Drawer
-              type="overlay"
-              content={<Text>Hello</Text>}
-              tapToClose={true}
-              openDrawerOffset={0.2}
-              panCloseMask={0.2}
-              closedDrawerOffset={-3}
-              styles={drawerStyles}
-              tweenHandler={(ratio) => ({
-                main: { opacity:(2-ratio)/2 }
-              })}
+              <SideMenu 
+                menu={<ControlPanel/>}
               >
-                <FlatList
-                  data={this.state.positions}
-                  extraData={this.state}
-                  keyExtractor={this._keyExtractor}
-                  renderItem={({item}) => <Text style={styles.item}>{item.quantity}</Text>}
-                />
-                <View style={styles.container}>
-                    <TouchableOpacity onPress={this._onPress}
-                        style={styles.button}
-                        activeOpacity={1}>
-                    </TouchableOpacity>
-                    <Animated.View style={[ styles.circle, {transform: [{scale: changeScale}]} ]} />
+                <View style={styles.backgroundContainer}>
+                  <View style={styles.contentContainer}>
+                    <Card>
+                      <VictoryBar/>
+                    </Card>
+                  </View>
+
+                  <View style={styles.contentContainer}>
+                    <Card>
+                      <FlatList
+                        data={this.state.positions}
+                        extraData={this.state}
+                        keyExtractor={this._keyExtractor}
+                        renderItem={({item}) => <Text style={styles.item}>{item.quantity}</Text>}
+                      />
+                    </Card>
+                  </View>
                 </View>
-            </Drawer>
+            </SideMenu>
                 
         );
     }
@@ -155,12 +132,19 @@ class AppScreen extends Component {
     }
 }
 
+const DEVICE_WIDTH = Dimensions.get('window').width;
+const DEVICE_HEIGHT = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        margin: 20,
-        alignItems: 'flex-end',
-        justifyContent: 'flex-end',
+    backgroundContainer: {
+      width: DEVICE_WIDTH,
+      height: DEVICE_HEIGHT,
+      backgroundColor: '#ffffff',
+    },
+    contentContainer: {
+      margin: 20,
+      marginTop: 40,
+      flex: 1,
     },
     button: {
         alignItems: 'center',
@@ -185,8 +169,20 @@ const styles = StyleSheet.create({
 });
 
 const drawerStyles = {
-  drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3},
-  main: {paddingLeft: 3},
+  drawer: { 
+    shadowColor: '#000000', 
+    shadowOpacity: 0.8, 
+    shadowRadius: 3,
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  main: {
+    paddingLeft: 3,
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
 }
 
 const mapStateToProps = (state) => ({
--- genie/src/components/ControlPanel.js
+++ genie/src/components/ControlPanel.js
@@ -4,31 +4,104 @@ import {
   StyleSheet,
   View,
   Text,
-  Button
+  Button,
+  Animated,
+  Image,
+  Easing
 } from 'react-native';
+import { Actions, ActionConst } from 'react-native-router-flux';
+
+import bgSrc from '../images/wallpaper.png';
+
+const SIZE = 40;
 
 export default class ControlPanel extends Component {
+  constructor() {
+      super();
+
+      this.state = {
+          isLoading: false
+      };
+
+      this._logout = this._logout.bind(this);
+      this.growAnimated = new Animated.Value(0);
+  }
+
+  _logout() {
+      if (this.state.isLoading) return;
+
+      this.setState({ isLoading: true });
+
+      Animated.timing(
+          this.growAnimated,
+          {
+              toValue: 1,
+              duration: 300,
+              easing: Easing.linear,
+          }
+      ).start();
+
+      setTimeout(() => {
+          Actions.pop();
+      }, 500);
+  }
+
   render() {
+    const changeScale = this.growAnimated.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, SIZE],
+        });
+
     return (
-      <View style={styles.controlPanel}>
-        <Text style={styles.controlPanelWelcome}>
-          Control Panel
-        </Text>
-        <Button
-          onPress={() => {
-            this.props.closeDrawer();
-          }}
-          text="Close Drawer"
-        />
-      </View>
+      <Image style={styles.picture} source={bgSrc}>
+         <View style={styles.controlPanel}>
+          <Text style={styles.controlPanelWelcome}>
+            Genie
+          </Text>
+          <Button
+            onPress={() => {
+              console.log("Home")
+            }}
+            title="Home"
+            style={styles.button}
+          />
+          <Button
+            onPress={() => {
+              console.log("Settings")
+            }}
+            title="Settings"
+            style={styles.button}
+          />
+          <Button
+            onPress={() => {
+              this._logout();
+            }}
+            title="Logout"
+            style={styles.button}
+          />
+          <Animated.View style={[ styles.circle, {transform: [{scale: changeScale}]} ]} />
+        </View>
+      </Image>
+        
     )
   }
 }
 
 const styles = StyleSheet.create({
+  picture: {
+      flex: 1,
+      width: null,
+      height: null,
+      resizeMode: 'cover',
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+  },
   controlPanel: {
     flex: 1,
-    backgroundColor:'#326945',
+    backgroundColor:'transparent',
+  },
+  button: {
+    backgroundColor: '#25c5a1'
   },
   controlPanelText: {
     color:'white',

058be03d2be952b6f21471aa9929b3c9466bf56c
Rudhra Raveendran <rudytheninja@gmail.com>
2017-09-10 06:52:51 -0400

    test

--- genie/src/components/ChatBotScreen.js
+++ genie/src/components/ChatBotScreen.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Dimensions, AppRegistry, StyleSheet, Text, View, Image, Navigator, TouchableHighlight, Button, TextInput } from 'react-native';
+import { Dimensions, KeyboardAvoidingView, AppRegistry, StyleSheet, Text, View, Image, Navigator, TouchableHighlight, Button, TextInput } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import ChatBot from 'react-native-chatbot';
-
+import KeyboardSpacer from 'react-native-keyboard-spacer';
 import { connect } from 'react-redux';
 import { Router, Scene, Actions, ActionConst } from 'react-native-router-flux';
 import SideMenu from 'react-native-side-menu'
 import ControlPanel from './ControlPanel'
+
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const DEVICE_WIDTH = Dimensions.get('window').width;
 const DEVICE_HEIGHT = Dimensions.get('window').height;
@@ -17,9 +19,11 @@ const MARGIN = 40;
 const accessToken = "af3aedbb148f460f9ca4754a3780fbae"
 const baseUrl = "https://api.api.ai/v1/"
 
+var msgid = 2;
+
 class ChatScreen extends React.Component {
-    async _getMessage( message ){
-      let reponse = await fetch(baseUrl + 'query', {
+    _getMessage( message ){
+      fetch(baseUrl + 'query', {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
@@ -32,43 +36,96 @@ class ChatScreen extends React.Component {
           sessionId: "genie"
         })
       })
-      let body = await response.json();
-      console.log(body);
-      return "test"
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log("res", responseJson.result.speech);
+        var messages = [
+          {
+            _id: ++msgid,
+            "createdAt": new Date(),
+            "text": responseJson.result.speech,
+            "user": {
+              "_id": 2,
+              name: 'Genie',
+              avatar: 'https://img.buzzfeed.com/buzzfeed-static/static/2016-01/21/13/campaign_images/webdr04/how-well-do-you-know-the-genie-from-aladdin-2-5437-1453401463-9_dblbig.jpg',
+            },
+          }
+        ]
+        this.setState((previousState) => ({
+          messages: GiftedChat.append(previousState.messages, messages),
+        }));
+      })
+      .catch((error) => {
+        console.error(error);
+        return 'ERROR'
+      });
     }
 
+    state = {
+    messages: [],
+  };
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Welcome to Genie. How can I help?',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Genie',
+            avatar: 'https://img.buzzfeed.com/buzzfeed-static/static/2016-01/21/13/campaign_images/webdr04/how-well-do-you-know-the-genie-from-aladdin-2-5437-1453401463-9_dblbig.jpg',
+          },
+        },
+        {
+          _id: 2,
+          image: 'https://media.giphy.com/media/v5jWoU9SBe6qY/giphy.gif',  
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Genie',
+            avatar: 'https://img.buzzfeed.com/buzzfeed-static/static/2016-01/21/13/campaign_images/webdr04/how-well-do-you-know-the-genie-from-aladdin-2-5437-1453401463-9_dblbig.jpg',
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this._getMessage(messages[0].text)
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
     render() {
-        const steps = [
-            {
-                id: '0',
-                message: 'Welcome to Genie. How can I help?',
-                trigger: '2',
-            },
-            {
-                id: '1',
-                message: ({ previousValue, steps }) => this._getMessage(previousValue),
-                trigger: '2'
-            },
-            {
-                id: '2',
-                user: true,
-                trigger: '1',
-            },
-        ];
         return (
           <SideMenu 
             menu={<ControlPanel/>}
           >
-            <ChatBot
-              steps={steps}
-              placeholder='Talk to Genie...'
-              botAvatar='https://img.buzzfeed.com/buzzfeed-static/static/2016-01/21/13/campaign_images/webdr04/how-well-do-you-know-the-genie-from-aladdin-2-5437-1453401463-9_dblbig.jpg'
-              botBubbleColor='#25c5a1'
-            />
+            <View style={styles.contentContainer}>
+              <GiftedChat
+                messages={this.state.messages}
+                onSend={(messages) => this.onSend(messages)}
+                user={{
+                  _id: 1,
+                }}
+              />
+              <KeyboardSpacer/>
+            </View>
           </SideMenu>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    contentContainer: {
+      padding: 10,
+      flex: 1,
+      backgroundColor: '#ffffff'
+    }
+});
 
 const mapStateToProps = (state) => ({
   token: state.token
